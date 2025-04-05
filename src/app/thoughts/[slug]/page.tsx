7fdeac6629547946f6e4a105f278c285b17fd1bd
xxKeefer <xxkeefer.code@gmail.com>
2025-04-05 14:41:55 +1000

    feat: :sparkles: blog header

--- src/app/thoughts/[slug]/page.tsx
+++ src/app/thoughts/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from 'next'
 
+import BlogHeader from '~/blog/BlogHeader'
 import { Blogs } from '~/blog/utils'
 
 type PageProps = {
@@ -8,15 +9,17 @@ type PageProps = {
 
 export default async function Page({ params }: PageProps) {
   const { slug } = await params
-  const blogs = await Blogs.init() // safe after init()
-  const blog = blogs.findBySlug(slug)
-  if (!blog) return <h1>404</h1>
+  const blogs = await Blogs.init()
+  // generateStaticParams should ensure existence, hence !
+  const blog = blogs.findBySlug(slug)!
   const { Content, meta } = blog
   console.log({ meta })
   return (
-    <main className="prose prose-sm sm:prose-base md:prose-lg lg:prose-2xl max-w-(--breakpoint-lg) p-4 sm:p-8 md:px-24 md:py-16 lg:mx-auto">
-      <Content />
-    </main>
+    <BlogHeader meta={meta}>
+      <article className="prose prose-sm sm:prose-base md:prose-lg lg:prose-2xl max-w-(--breakpoint-lg) p-4 sm:p-8 md:px-24 md:py-16 lg:mx-auto">
+        <Content />
+      </article>
+    </BlogHeader>
   )
 }
 
@@ -31,7 +34,7 @@ export async function generateMetadata({
   params,
 }: PageProps): Promise<Metadata> {
   const { slug } = await params
-  const blogs = await Blogs.init() // safe after init()
+  const blogs = await Blogs.init()
   const post = blogs.findBySlug(slug)
   return Blogs.toNextMetadata(post!.meta)
 }
---
+++ src/blog/BlogHeader.tsx
@@ -0,0 +1,89 @@
+import {
+  BookOpenText,
+  CalendarBlank,
+  DotOutline,
+  PencilLine,
+} from '@phosphor-icons/react/dist/ssr'
+import { format, minutesToSeconds } from 'date-fns'
+import React, { ReactNode } from 'react'
+
+import { Meta } from './utils'
+
+type Props = {
+  meta: Meta
+  children: ReactNode
+}
+
+const BlogHeader = ({ meta, children }: Props) => {
+  const {
+    title,
+    readingTimeMinutes: mins,
+    description,
+    publishedDate,
+    tags,
+    author,
+    updatedDate,
+  } = meta
+  const date = format(updatedDate ?? publishedDate, 'iii, ii MMM yyyy')
+  const seconds = mins % 1
+  const minutes = Math.floor(mins)
+  const readTime = `~ ${minutes}m ${minutesToSeconds(seconds)}s`
+  return (
+    <main>
+      <div className="bg-base-300">
+        <div className="prose md:prose-2xl max-w-(--breakpoint-lg) p-4 pt-8 sm:p-8 md:px-24 md:pt-16 md:pb-8 lg:mx-auto">
+          <div className="flex-col place-items-start">
+            <h1 className="mb-8 text-5xl font-black tracking-wide md:text-7xl">
+              {title}
+            </h1>
+            <p className="font-display not-dark:bg-neutral not-dark:text-neutral-content mb-4 text-xl tracking-wider not-dark:rounded-2xl not-dark:px-4 not-dark:py-2 md:text-3xl">
+              <span className="font-emoji pr-2">🧙‍♂️✨</span>
+              {description}
+            </p>
+            <p className="2xs:gap-2 mb-2 flex justify-items-end gap-4 justify-self-end text-xs sm:gap-4 md:text-sm">
+              <span className="flex content-center gap-2">
+                <PencilLine
+                  className="hidden sm:block"
+                  size={16}
+                  weight="bold"
+                />
+                {author}
+              </span>
+              <DotOutline className="2xs:block hidden sm:hidden" size={16} />
+              <span className="flex content-center gap-2">
+                <CalendarBlank
+                  className="hidden sm:block"
+                  size={16}
+                  weight="bold"
+                />
+                {date}
+              </span>
+              <DotOutline className="2xs:block hidden sm:hidden" size={16} />
+              <span className="flex content-center gap-2">
+                <BookOpenText
+                  className="hidden sm:block"
+                  size={16}
+                  weight="bold"
+                />
+                {readTime}
+              </span>
+            </p>
+            <div className="2xs:flex-nowrap 2xs:gap-4 flex flex-wrap justify-items-end gap-2 justify-self-end">
+              {tags.map((tag) => (
+                <div
+                  key={tag}
+                  className="badge badge-xs 2xs:badge-sm md:badge-md badge-accent first-of-type:badge-primary font-mono tracking-wider"
+                >
+                  {tag}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+      {children}
+    </main>
+  )
+}
+
+export default BlogHeader
